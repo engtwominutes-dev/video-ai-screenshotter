95bfb416839b849963af8dc4ede7bc469df0a7e4
engtwominutes-dev <engtwominutes@gmail.com>
2026-01-04 13:10:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
